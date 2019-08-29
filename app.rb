e1db9a9c72a930dcc6b798dfbe6c255c0a474edc
kevin lang <langkevina@gmail.com>
2019-08-29 01:06:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,14 @@ class App < Sinatra::Base
   get '/' do
      "Hello World"
    end
-   
+
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    reversed_string = original_string.reverse
     erb :reversed
   end
 
